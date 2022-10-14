214ed6c2a7d227eb6a4bc1471a9963051798dc4d
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2022-10-14 18:07:43 +0300

    Plans buttons JS added

---
+++ js/shared.js
@@ -0,0 +1,13 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButton = document.querySelectorAll('.plan button');
+
+// console.dir(backdrop);
+// console.dir(selectPlanButton);
+
+for (var i = 0; i < selectPlanButton.length; i++) {
+    selectPlanButton[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
